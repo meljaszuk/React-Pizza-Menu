28caaa38f530d68958dfcf1019cbe3e39c4efe56
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-05-23 09:17:45 +0200

    improving components structure

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const pizzaData = [
     name: "Focaccia",
     ingredients: "Bread with italian olive oil and rosemary",
     price: 6,
-    photoName: "pizzas/focaccia.jpg",
+    photoName: "../public/pizzas/focaccia.jpg",
     soldOut: false,
   },
   {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,52 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import {pizzaData} from"./data.js";
+/* import {pizzaData} from"./data.js"; */
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "../public/pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
 
 function App() {
   return (
@@ -65,29 +110,29 @@ function Pizza({ pizzaObj }) {
 }
 
 function Footer() {
-    const now = new Date();
-    const openHour = 12;
-    const closeHour = 24;
-  
-    const currentHour = now.getHours();
-    const currentMinute = now.getMinutes();
-  
-    const isOpen = (currentHour >= openHour && (currentHour < closeHour || (currentHour === closeHour && currentMinute === 0)));
-  
-    console.log(isOpen);
-  
-    return (
-      <footer className="footer">
-        {isOpen ? (
-          <Order closeHour={closeHour} openHour={openHour} />
-        ) : (
-          <p>
-            We're happy to welcome you between {openHour}:00 and {closeHour}:00.
-          </p>
-        )}
-      </footer>
-    );
-  }
+  const now = new Date();
+  const openHour = 12;
+  const closeHour = 24;
+
+  const currentHour = now.getHours();
+  const currentMinute = now.getMinutes();
+
+  const isOpen = (currentHour >= openHour && (currentHour < closeHour || (currentHour === closeHour && currentMinute === 0)));
+
+  console.log(isOpen);
+
+  return (
+    <footer className="footer">
+      {isOpen ? (
+        <Order closeHour={closeHour} openHour={openHour} />
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
+      )}
+    </footer>
+  );
+}
 
 function Order({ closeHour, openHour }) {
   return (
